83e2e76ef0437e4ccc6e6cb9132453191a0edf10
zeta1999 <renoir42@yahoo.com>
2021-08-21 02:22:20 +0900

    fix

--- encoders/iencoder.cc
+++ encoders/iencoder.cc
@@ -35,31 +35,35 @@ namespace moetsi::ssp {
         std::string codec_parameters_file = std::string(config);
         YAML::Node codec_parameters = YAML::LoadFile(codec_parameters_file);
         for (FrameType type : types) {
-            YAML::Node v = codec_parameters["video_encoder"][unsigned(type)];
+            try {
+                YAML::Node v = codec_parameters["video_encoder"][unsigned(type)];
 
-            std::string encoder_type = v["type"].as<std::string>();
-            std::shared_ptr<IEncoder> fe;
-            if (encoder_type == "libav")
-                fe = std::shared_ptr<LibAvEncoder>(new LibAvEncoder(v, v["fps"].as<int>()));
-            else if (encoder_type == "nvenc") {
-    #ifdef SSP_WITH_NVPIPE_SUPPORT
-                fe = std::shared_ptr<NvEncoder>(new NvEncoder(v, v["fps"].as<int>()));
-    #else
-                spdlog::error("SSP compiled without \"nvenc\" reader support. Set to "
-                            "SSP_WITH_NVPIPE_SUPPORT=ON when configuring with cmake");
-                return encoders;
-    #endif
-            } else if (encoder_type == "zdepth")
-                fe = std::shared_ptr<ZDepthEncoder>(new ZDepthEncoder(v, v["fps"].as<int>()));
-            else if (encoder_type == "null")
-                fe = std::shared_ptr<NullEncoder>(new NullEncoder(v["fps"].as<int>()));
-            else {
-                spdlog::error("Unknown encoder type: \"{}\". Supported types are "
-                            "\"libav\", \"nvenc\", \"zdepth\" and \"null\"",
-                            encoder_type);
-                return encoders;
+                std::string encoder_type = v["type"].as<std::string>();
+                std::shared_ptr<IEncoder> fe;
+                if (encoder_type == "libav")
+                    fe = std::shared_ptr<LibAvEncoder>(new LibAvEncoder(v, v["fps"].as<int>()));
+                else if (encoder_type == "nvenc") {
+        #ifdef SSP_WITH_NVPIPE_SUPPORT
+                    fe = std::shared_ptr<NvEncoder>(new NvEncoder(v, v["fps"].as<int>()));
+        #else
+                    spdlog::error("SSP compiled without \"nvenc\" reader support. Set to "
+                                "SSP_WITH_NVPIPE_SUPPORT=ON when configuring with cmake");
+                    return encoders;
+        #endif
+                } else if (encoder_type == "zdepth")
+                    fe = std::shared_ptr<ZDepthEncoder>(new ZDepthEncoder(v, v["fps"].as<int>()));
+                else if (encoder_type == "null")
+                    fe = std::shared_ptr<NullEncoder>(new NullEncoder(v["fps"].as<int>()));
+                else {
+                    spdlog::error("Unknown encoder type: \"{}\". Supported types are "
+                                "\"libav\", \"nvenc\", \"zdepth\" and \"null\"",
+                                encoder_type);
+                    return encoders;
+                }
+                encoders[type] = fe;
+            } catch(std::exception &e) {
+                spdlog::error("skipping codec {} : {}", unsigned(type), e.what());
             }
-            encoders[type] = fe;
         }
 
         return encoders;
